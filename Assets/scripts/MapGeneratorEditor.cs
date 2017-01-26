a0625b06461cd7de0374bb551c0b79a9d66fa154
Adrián Villanueva <adrianvillanueva997@gmail.com>
2017-01-26 10:05:57 +0100

    First 2D Version

---
+++ Assets/scripts/MapGeneratorEditor.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(GenerateMap))]
+public class MapGeneratorEditor : Editor
+{
+
+    public override void OnInspectorGUI()
+    {
+        GenerateMap mapGen = (GenerateMap)target;
+
+        if (DrawDefaultInspector())
+        {
+            if (mapGen.autoUpdate)
+            {
+                mapGen.mapGenerate();
+            }
+        }
+
+        if (GUILayout.Button("Generate"))
+        {
+            mapGen.mapGenerate();
+        }
+    }
+}
